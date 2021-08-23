b0efbbc534c579cf9ea0be84fd3102341f0880cb
kricsleo <kriclseo@163.com>
2021-08-23 15:30:20 +0800

    init template

---
+++ template/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <my-comp />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import MyComp from "./components/MyComp.vue";
+
+export default Vue.extend({
+  name: "app",
+  components: {
+    MyComp
+  }
+});
+</script>
+
+<style lang="scss"></style>
---
+++ template/src/components/MyComp.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="my-comp"></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "my-comp"
+});
+</script>
+
+<style scoped lang="scss"></style>
